73f1a3e815b94c38de3ab0056317caed66b41379
JunyaUkyou <ukyou.junya0514@gmail.com>
2026-05-01 09:27:09 +0900

    fix warinig of tailwind

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,10 +8,10 @@ export const Home = () => {
       <div className="absolute inset-0 pointer-events-none" aria-hidden="true">
         <div className="absolute -top-20 -right-20 w-80 h-80 rounded-full bg-green-light opacity-40" />
         <div className="absolute -bottom-16 -left-16 w-64 h-64 rounded-full bg-ceramic opacity-60" />
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] rounded-full bg-green-light opacity-15" />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[150] h-[150] rounded-full bg-green-light opacity-15" />
       </div>
 
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-[420px] px-6 py-16 md:py-24">
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-[105] px-6 py-16 md:py-24">
         {/* Heading */}
         <h1 className="text-starbucks-green text-4xl md:text-5xl lg:text-6xl font-semibold tracking-tight text-center leading-tight max-w-3xl">
           Hover Button Styles
