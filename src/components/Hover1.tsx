ed0c992cb8ecc67856a054117dfa1f5f0e829ef4
JunyaUkyou <ukyou.junya0514@gmail.com>
2026-05-02 11:43:16 +0900

    set style

--- src/components/Hover1.tsx
+++ src/components/Hover1.tsx
@@ -1,9 +1,13 @@
 export const Hover1 = () => {
   return (
-    <div className="flex justify-center items-center py-5 rounded-lg bg-neutral-warm/60 border border-ceramic mb-5 group">
-      <a
-        href="#"
-        className="no-underline text-lime-900 border-solid border-4 py-10 px-20 relative 
+    <div className="flex justify-center items-center py-5 rounded-lg bg-neutral-warm/60 border border-ceramic mb-5">
+      <div className="group">
+        <a
+          href="#"
+          className="no-underline text-lime-900 border-solid border-4 py-2 px-5 relative block group-hover:before:rotate-0
+        z-0
+        rounded-full
+        overflow-hidden
         before:content-['']
         before:absolute
         before:left-0
@@ -11,12 +15,16 @@ export const Hover1 = () => {
         before:bg-red-100
         before:w-full
         before:h-full
-        before:-z-1
-        group-hover:bg-black
+        before:-z-10
+        before:origin-bottom-left
+        before:-rotate-90
+        before:transition-[rotate]
+        before:duration-300
         "
-      >
-        aaaa
-      </a>
+        >
+          Hover Me
+        </a>
+      </div>
     </div>
   );
 };
