28bdbf41dce0f707a66a506faf8d1843a82c54c4
JunyaUkyou <ukyou.junya0514@gmail.com>
2026-05-03 21:23:18 +0900

    create overButton2

---
+++ src/components/Hover2.tsx
@@ -0,0 +1,31 @@
+export const Hover2 = () => {
+  return (
+    <div className="flex justify-center items-center py-5 rounded-lg bg-neutral-warm/60 border border-ceramic mb-5">
+      <div className="group">
+        <a
+          className="no-underline  py-2 px-5 relative block group-hover:before:translate-x-0
+          text-white
+          text-sm
+          bg-gold
+          overflow-hidden
+        z-0
+        rounded-full
+        before:content-['']
+        before:absolute
+        before:left-0
+        before:top-0
+        before:bg-starbucks-green
+        before:w-full
+        before:h-full
+        before:-z-10
+        before:-translate-x-full
+        before:transition-[translate]
+        before:duration-300
+        "
+        >
+          Hover Me
+        </a>
+      </div>
+    </div>
+  );
+};
